ab74dce66f249edbe5345a03306c4d37a1dab7a9
Felixmax_ <maxmp3l5i@gmail.com>
2025-07-26 20:58:28 +0200

    improved list building

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// serve static files from the 'static' directory on /static/
+app.use('/static', express.static('static'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/webfiles/index.html');
+    });
+
+    app.get('/scan', (req, res) => {
+    res.sendFile(__dirname + '/webfiles/scan.html');
+    });
+
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
